05a950d2166c096271cff1c06fd961056edf4307
hackclubium <hiddenhorseywriter@proton.me>
2026-07-04 17:34:38 -0400

    Add Braize YSWS site

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// Deployed to https://hackclubium.github.io/Braize/
+// If a custom domain is added later, change `base` to '/'.
+export default defineConfig({
+  site: 'https://hackclubium.github.io',
+  base: '/Braize',
+});
